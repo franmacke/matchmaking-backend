a9adfbf1174ac2fb7ed1b244d4939514cc3b8590
franmacke <fmacke10@gmail.com>
2024-04-19 15:52:11 -0300

    Add CORS headers configuration to settings.py

--- matchmaking/settings.py
+++ matchmaking/settings.py
@@ -43,15 +43,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'drf_yasg',
     'api.apps.ApiConfig',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True   
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
